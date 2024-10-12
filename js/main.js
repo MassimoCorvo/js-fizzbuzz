b84cd263058529995e8a02625fdceff172a56adf
MassimoCorvo <massimocorvo22@gmail.com>
2024-10-12 14:41:33 +0200

    print on page version

--- js/main.js
+++ js/main.js
@@ -1,22 +1,34 @@
 //console.log("collegato");
 
+const numbers = document.getElementById("numbers");
+
 for(let i = 1; i <= 100; i++){
     //console.log(i);
 
     // Caso 1: multipli di 3 e di 5
     if( (i%3) === 0 && (i%5) === 0){
-        console.log("FizzBuzzz");
+        //console.log("FizzBuzzz");
+
+        numbers.innerHTML += "<div>FizzBuzz</div>";
+
+
     }
     // Caso 2: multipli di 3
      else if ((i%3) === 0){
-        console.log("Fizz");
+        //console.log("Fizz");
+
+        numbers.innerHTML += "<div>Fizz</div>";
     }
     // Caso 3: multipli di 5
      else if ((i%5) === 0){
-        console.log("Buzz");
+        //console.log("Buzz");
+
+        numbers.innerHTML += "<div>Buzz</div>";
     }
     // Tutti gli altri numeri 
     else{
-        console.log(i);
+        //console.log(i);
+
+        numbers.innerHTML += "<div>" + i + "</div>";
     }
 }
