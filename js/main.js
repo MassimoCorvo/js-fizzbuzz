e365b8ab89f10af55b3f425cdea3cccb1a36df53
MassimoCorvo <massimocorvo22@gmail.com>
2024-10-12 16:29:27 +0200

    changes to js code

--- js/main.js
+++ js/main.js
@@ -4,33 +4,36 @@ const numbers = document.getElementById("numbers");
 
 for(let i = 1; i <= 100; i++){
     //console.log(i);
+    let print;
 
     // Caso 1: multipli di 3 e di 5
     if( (i%3) === 0 && (i%5) === 0){
         //console.log("FizzBuzzz");
 
-        numbers.innerHTML += "<tr><td>" + i + "</td><td>FizzBuzz</td></tr>";
-
+        print = "FizzBuzz";
 
     }
     // Caso 2: multipli di 3
      else if ((i%3) === 0){
         //console.log("Fizz");
 
-        numbers.innerHTML += "<tr><td>" + i + "</td><td>Fizz</td></tr>";
+        print = "Fizz";
+
     }
     // Caso 3: multipli di 5
      else if ((i%5) === 0){
         //console.log("Buzz");
 
-        numbers.innerHTML += "<tr><td>" + i + "</td><td>Buzz</td></tr>";
+        print = "Buzz";
     }
     // Tutti gli altri numeri 
     else{
         //console.log(i);
 
-        numbers.innerHTML += "<tr><td>" + i + "</td><td>" + i + "</td></tr>";
+        print = i;
     }
+
+    numbers.innerHTML += "<tr><td>" + i + "</td><td>" + print + "</td></tr>";
 }
 
 //Tabella a righe colorate alternate
